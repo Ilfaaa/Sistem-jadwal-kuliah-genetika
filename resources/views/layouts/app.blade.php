b3407a12d00ab38c60cdb5268a762a3981d5a6f3
Ilfaaa <ilfaerdin5138@gmail.com>
2026-06-23 23:13:18 +0700

    Add geometric floating ornaments (triangle, square, pentagon, circle, cross) and slow floating animations to background

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,17 @@
 @include('layouts.partials.navbar')
 
 <div class="content-wrapper content-theme">
+    <!-- Floating Background Ornaments -->
+    <div class="bg-ornaments-wrapper">
+        <div class="bg-ornament shape-triangle shape-1"></div>
+        <div class="bg-ornament shape-square shape-2"></div>
+        <div class="bg-ornament shape-pentagon shape-3"></div>
+        <div class="bg-ornament shape-circle shape-4"></div>
+        <div class="bg-ornament shape-triangle shape-5"></div>
+        <div class="bg-ornament shape-square shape-6"></div>
+        <div class="bg-ornament shape-cross shape-7"></div>
+        <div class="bg-ornament shape-pentagon shape-8"></div>
+    </div>
     @yield('content')
 </div>
 
